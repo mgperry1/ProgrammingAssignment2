d6c9e37d9d81b061ca3a91e9d067eac5e1f0b501
mgperry1 <matt_perry1@yahoo.com>
2014-08-21 10:31:06 -0400

    Code Works

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,34 @@
-## Put comments here that give an overall description of what your
+## Put comments here that give an overall description 
+#of what your
 ## functions do
 # t<-makeCacheMatrix()
-# t$set(matrix(1:6,3,3)to store a matrix
+# t$set(matrix(1:4,2,2))to store a matrix
 # cacheSolve(t)                                                                             
-                                                                             When I run cacheSolve(a) I get Error in as.vector(x, mode) : cannot coerce type 'closure' to vector of type 'any'
 
-## Write a short comment describing this function
+## Setup the Matrix object
 makeCacheMatrix <- function(x = matrix()) {
-  m<-NULL
+ m<-NULL
   set<-function(y){
     x<<-y
     m<<-NULL
   }
   get<-function() x
-  setmatrix<-function(solve) m<<- solve
-  getmatrix<-function() m
-  list(set=set, get=get,
-       setmatrix=setmatrix,
-       getmatrix=getmatrix)
+    setmatrix<-function(solve) m<<- solve
+    getmatrix<-function() m
+     list(set=set, get=get,
+           setmatrix=setmatrix,
+            getmatrix=getmatrix)
 }
 
-## Write a short comment describing this function
 ## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x=matrix(), ...) {
-  m<-x$getmatrix()
-  if(!is.null(m)){
-    message("getting cached data")
-    return(m)
-  }
-  matrix<-x$get()
-  m<-solve(matrix, ...)
-  x$setmatrix(m)
-  m
+      m<-x$getmatrix()
+      if(!is.null(m)){
+         message("getting cached data")
+         return(m)
+       }
+       matrix<-x$get()
+       m<-solve(matrix, ...)
+       x$setmatrix(m)
+       m
 }
